022749eb6d7b78a7dfb72996f574d6fb92fca4b4
Jose Zagalia <jzagalia@live.com.ar>
2023-01-16 16:01:20 -0300

    Primera escritura en el repositorio local

---
+++ sintaxis.js
@@ -0,0 +1,15 @@
+
+document.getElementById("boton").onclick = function () {
+    console.log("capturamos evento");
+    document.getElementById("demo").innerHTML = "John Smith"
+}
+
+document.getElementById("tombo").onclick = function () {
+    console.log("capturamos evento");
+    document.getElementById("edad").innerHTML = "48 años"
+}
+
+document.getElementById("ciudades").onclick = function () {
+    console.log("capturamos evento");
+    document.getElementById("ciudad").innerHTML = "Campana"
+}
